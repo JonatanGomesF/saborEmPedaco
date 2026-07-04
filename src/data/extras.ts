d5ac7b4dc3ada0de76c5e2091a4105b2ec6cc3a6
jonatan gomes floriano <160983484+jonatancolt@users.noreply.github.com>
2026-07-04 15:01:20 -0300

    remove extras

--- src/data/extras.ts
+++ src/data/extras.ts
@@ -1,20 +1,20 @@
 export const extras = [
   {
     id: 1,
-    name: "Raspas de Gengibre",
-    price: 0,
+    name: "Molho de Pimenta",
+    price: 3.00,
     category: "Adicionais",
   },
   {
     id: 2,
-    name: "Pimentão Vermelho",
-    price: 0,
+    name: "queijo ralado",
+    price: 5.00,
     category: "Adicionais",
   },
   {
     id: 3,
-    name: "Carne Extra (80g)",
-    price: 9.9,
+    name: "Molho de Alho",
+    price: 2.00,
     category: "Adicionais",
   },
   {
@@ -25,8 +25,8 @@ export const extras = [
   },
   {
     id: 5,
-    name: "Camarão Extra (4 unid)",
-    price: 12.9,
+    name: "alho poró",
+    price: 6.00,
     category: "Adicionais",
   },
   {
@@ -34,36 +34,36 @@ export const extras = [
     name: "Coca Cola caçulinha 227ml",
     price: 4.0,
     category: "Refrigerantes",
-  },
+  },]
   // Sushi / Hot specific extras
-  {
-    id: 7,
-    name: "Tarê",
-    price: 3.0,
-    category: "Sushi",
-  },
-  {
-    id: 8,
-    name: "Gergelim",
-    price: 0,
-    category: "Sushi",
-  },
-  {
-    id: 9,
-    name: "Cream Cheese Extra",
-    price: 4.5,
-    category: "Sushi",
-  },
-  {
-    id: 10,
-    name: "Porção de 5 un. extra",
-    price: 12.0,
-    category: "Sushi",
-  },
-  {
-    id: 11,
-    name: "Shoyu",
-    price: 2.0,
-    category: "Sushi",
-  },
-];
+//   {
+//     id: 7,
+//     name: "Tarê",
+//     price: 3.0,
+//     category: "Sushi",
+//   },
+//   {
+//     id: 8,
+//     name: "Gergelim",
+//     price: 0,
+//     category: "Sushi",
+//   },
+//   {
+//     id: 9,
+//     name: "Cream Cheese Extra",
+//     price: 4.5,
+//     category: "Sushi",
+//   },
+//   {
+//     id: 10,
+//     name: "Porção de 5 un. extra",
+//     price: 12.0,
+//     category: "Sushi",
+//   },
+//   {
+//     id: 11,
+//     name: "Shoyu",
+//     price: 2.0,
+//     category: "Sushi",
+//   },
+// ];
